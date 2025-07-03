e71a0445e74ff699a5eb660c6a01b81f2a6067f9
Sam <sambhavchugh2004@gmail.com>
2025-07-04 00:28:16 +0530

    Add backend and frontend package.json files with build and start scripts

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,12 +6,19 @@ import cookieParser from "cookie-parser"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 import cors from 'cors'
+import path from "path"
+import { fileURLToPath } from 'url';
 
 
 dotenv.config({path: './.env'});
 
 const app = express();
 const PORT = process.env.PORT
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -29,6 +36,18 @@ app.use("/api/users", userRoutes)
 app.use("/avatars", express.static("public/avatars"));
 
 app.use("/api/chat", chatRoutes)
+
+
+if (process.env.NODE_ENV === "production") {
+  const distPath = path.resolve(__dirname, "../../frontend/dist");
+
+  app.use(express.static(distPath));
+
+  app.get(/^\/(?!api).*/, (req, res) => {
+
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+}
 
 
 
